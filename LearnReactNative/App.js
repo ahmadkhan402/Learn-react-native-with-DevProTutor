e07896fba6f8bb2443afe691386c895e83be027d
Ahmad Saffiullah <sadapukhtoon777@gmail.com>
2024-03-29 21:29:57 +0500

    Installation

---
+++ LearnReactNative/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{color:"#fff" , fontWeight:"bold",fontSize:20}}>Hello Developer</Text>
+      <Text style={{color:"#fff",fontWeight:"300", fontSize:15}}>lets Start React Native From Zero to Master</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
